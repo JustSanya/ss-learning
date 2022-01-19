298b2fba741ee432df56a2aa2da5c1f0aa3ac00c
Oleksandr Shyrykalov <oshyryk@softserveinc.com>
2022-01-19 11:23:13 +0200

    seems working

--- src/app.ts
+++ src/app.ts
@@ -1,97 +1,22 @@
-// import { GateState, stateMap } from "./types";
-
-// const gateStatusIndicator = document.getElementById(
-//   "status"
-// ) as HTMLParagraphElement;
-const countdown = document.getElementById("countdown") as HTMLParagraphElement;
-// const toggle = document.getElementById("toggle") as HTMLButtonElement;
-
-// class SignalReceiver {
-//   private gatewayController = new GatewayController();
-
-//   callController() {
-//     this.gatewayController.toggle();
-//   }
-// }
-
-// class GatewayController {
-//   private gate: Gate = new Gate();
-
-//   toggle(): void {
-//     switch (this.gate.currentState) {
-//       case GateState.CLOSED:
-//         this.gate.open();
-//         break;
-//       case GateState.OPENED:
-//         this.gate.close();
-//         break;
-//       case GateState.OPENING:
-//       case GateState.CLOSING:
-//         this.gate.pause();
-//         break;
-//       case GateState.PENDING:
-//         this.gate.reverseAction();
-//         break;
-//     }
-//   }
-// }
-
-// class Gate {
-//   constructor() {
-//     this.finishAction = this.finishAction.bind(this);
-//   }
-
-//   currentState: GateState = GateState.CLOSED;
-//   private timer = new Timer();
-//   private cachedState: GateState | null = null;
-
-//   open(): void {
-//     this.setState(GateState.OPENING);
-//     this.timer.initialize(this.finishAction, GateState.OPENED);
-//   }
-
-//   close(): void {
-//     this.setState(GateState.CLOSING);
-//     this.timer.initialize(this.finishAction, GateState.CLOSED);
-//   }
-
-//   finishAction(state: GateState): void {
-//     this.setState(state);
-//     this.timer.reset();
-//   }
-
-//   pause(): void {
-//     this.cachedState = this.currentState;
-//     this.setState(GateState.PENDING);
-//     this.timer.pause();
-//   }
-
-//   reverseAction(): void {
-//     this.timer.reverse();
-//     this.cachedState === GateState.OPENING ? this.close() : this.open();
-//     this.cachedState = null;
-//   }
-
-//   setState(state: number): void {
-//     this.currentState = state;
-//     gateStatusIndicator.textContent = stateMap[state];
-//   }
-// }
-
 class Timer {
   private DEFAULT_MS_TIMEOUT = 10000;
   private DEFAULT_MS_STEP = 1000;
+  protected context: GateContext | undefined;
 
-  interval: number | undefined = undefined;
+  public interval: number | undefined = undefined;
   private remaining: number = this.DEFAULT_MS_TIMEOUT;
 
-  initialize(callback: Function, state: GateState): void {
+  constructor(context: GateContext) {
+    this.context = context;
+  }
+
+  initialize(): void {
     this.interval = window.setInterval(() => {
       if (this.remaining > 0) {
         this.remaining -= this.DEFAULT_MS_STEP;
-        countdown.textContent = (this.remaining / 1000).toString();
+        console.log(`Time remaining: ${this.remaining}`);
       } else {
-        callback(state);
+        this.context?.finishAction();
       }
     }, this.DEFAULT_MS_STEP);
   }
@@ -99,11 +24,11 @@ class Timer {
   reset(): void {
     this.remaining = this.DEFAULT_MS_TIMEOUT;
     window.clearInterval(this.interval);
-    countdown.textContent = (this.remaining / 1000).toString();
   }
 
   reverse() {
     this.remaining = this.DEFAULT_MS_TIMEOUT - this.remaining;
+    this.initialize();
   }
 
   pause() {
@@ -112,23 +37,14 @@ class Timer {
   }
 }
 
-// const signalReceiver = new SignalReceiver();
-
-// toggle.addEventListener(
-//   "click",
-//   () => {
-//     signalReceiver.callController();
-//   },
-//   false
-// );
-
 class GateContext {
   private state: GateState | undefined;
-  private timer: Timer = new Timer();
+  public timer: Timer | undefined;
   public cachedDirection: "opening" | "closing" | null = null;
 
   constructor(state: GateState) {
     this.transitionTo(state);
+    this.timer = new Timer(this);
   }
 
   public transitionTo(state: GateState): void {
@@ -136,10 +52,30 @@ class GateContext {
     this.state = state;
     this.state?.setContext(this);
     this.state?.connectTimer(this.timer);
+
+    if (state instanceof OpeningGate) {
+      this.cachedDirection = "opening";
+    } else if (state instanceof ClosingGate) {
+      this.cachedDirection = "closing";
+    } else if (!(state instanceof PausedGate)) {
+      this.cachedDirection = null;
+    }
   }
 
   public toggle(): void {
     this.state?.toggle();
+  }
+
+  public finishAction(): void {
+    if (this.cachedDirection === "opening") {
+      this.transitionTo(new OpenedGate());
+      console.log("Gate opened");
+      this.timer?.reset();
+    } else if (this.cachedDirection === "closing") {
+      this.transitionTo(new ClosedGate());
+      console.log("Gate closed");
+      this.timer?.reset();
+    }
   }
 }
 
@@ -151,7 +87,7 @@ abstract class GateState {
     this.context = context;
   }
 
-  public connectTimer(timer: Timer) {
+  public connectTimer(timer: Timer | undefined) {
     this.timer = timer;
   }
 
@@ -162,12 +98,14 @@ class OpenedGate extends GateState {
   public toggle(): void {
     console.log("start closing gates");
     this.context?.transitionTo(new ClosingGate());
+    this.context?.timer?.initialize();
   }
 }
 
 class OpeningGate extends GateState {
   public toggle(): void {
     console.log("Paused gate");
+    this.timer?.pause();
     this.context?.transitionTo(new PausedGate());
   }
 }
@@ -177,9 +115,11 @@ class PausedGate extends GateState {
     if (this.context?.cachedDirection === "opening") {
       console.log("now closing gate");
       this.context?.transitionTo(new ClosingGate());
+      this.timer?.reverse();
     } else if (this.context?.cachedDirection === "closing") {
       console.log("now opening gate");
       this.context?.transitionTo(new OpeningGate());
+      this.timer?.reverse();
     }
   }
 }
@@ -187,6 +127,7 @@ class PausedGate extends GateState {
 class ClosingGate extends GateState {
   public toggle(): void {
     console.log("Paused gate");
+    this.timer?.pause();
     this.context?.transitionTo(new PausedGate());
   }
 }
@@ -195,8 +136,21 @@ class ClosedGate extends GateState {
   public toggle(): void {
     console.log("start opening gates");
     this.context?.transitionTo(new OpeningGate());
+    this.context?.timer?.initialize();
   }
 }
 
 const gate = new GateContext(new ClosedGate());
+
 gate.toggle();
+window.setTimeout(() => {
+  gate.toggle();
+}, 3000);
+
+window.setTimeout(() => {
+  gate.toggle();
+}, 5000);
+
+window.setTimeout(() => {
+  gate.toggle();
+}, 10000);
