dc27f5ea54d3f0f530612179b9356e18151c2a02
Oleksandr Shyrykalov <oshyryk@softserveinc.com>
2022-01-28 20:39:43 +0200

    configure jest

---
+++ src/CarSensor.test.ts
@@ -0,0 +1,19 @@
+import CarSensor from "./CarSensor";
+
+test("should notify about car arriving", () => {
+  const carSensor = new CarSensor();
+  const spy = jest.spyOn(carSensor, "notify");
+
+  carSensor.onCarArrived();
+
+  expect(spy).toHaveBeenCalledWith("CAR_ARRIVED");
+});
+
+test("should notify about car leaving", () => {
+  const carSensor = new CarSensor();
+  const spy = jest.spyOn(carSensor, "notify");
+
+  carSensor.onCarLeft();
+
+  expect(spy).toHaveBeenCalledWith("CAR_LEFT");
+});
