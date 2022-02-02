03469b119c5900dc3defe0c687c0e46e827c89d1
Oleksandr Shyrykalov <oshyryk@softserveinc.com>
2022-02-02 13:10:34 +0200

    fix event manager test

--- __tests__/EventManager.test.ts
+++ __tests__/EventManager.test.ts
@@ -7,20 +7,26 @@ class MockObserver implements Observer {
   }
 }
 
+class EventManagerTestable extends EventManager {
+  public getObserversQuantity() {
+    return this.observers.length;
+  }
+}
+
 describe("event manager as a subject of observation", () => {
   it("should register observers", () => {
-    const eventManager = new EventManager();
+    const eventManager = new EventManagerTestable();
     const observer1 = new MockObserver();
     const observer2 = new MockObserver();
 
     eventManager.subscribe(observer1);
     eventManager.subscribe(observer2);
 
-    expect(eventManager["observers"].length).toEqual(2);
+    expect(eventManager.getObserversQuantity()).toEqual(2);
   });
 
   it("should unregister observers", () => {
-    const eventManager = new EventManager();
+    const eventManager = new EventManagerTestable();
     const observer1 = new MockObserver();
     const observer2 = new MockObserver();
 
@@ -30,17 +36,17 @@ describe("event manager as a subject of observation", () => {
     eventManager.unsubscribe(observer1);
     eventManager.unsubscribe(observer2);
 
-    expect(eventManager["observers"].length).toEqual(0);
+    expect(eventManager.getObserversQuantity()).toEqual(0);
   });
 
   it("should not add same observer twice", () => {
-    const eventManager = new EventManager();
+    const eventManager = new EventManagerTestable();
 
     const observer1 = new MockObserver();
 
     eventManager.subscribe(observer1);
     eventManager.subscribe(observer1);
 
-    expect(eventManager["observers"].length).toEqual(1);
+    expect(eventManager.getObserversQuantity()).toEqual(1);
   });
 });
